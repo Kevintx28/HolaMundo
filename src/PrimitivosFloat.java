56a29d1f8ba7f020b44598d1ce569784b36fe197
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-11 18:27:44 -0500

    Primitivos Numericos depunto flotante

---
+++ src/PrimitivosFloat.java
@@ -0,0 +1,7 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+        
+        float realFloat = 1.5e-10f ;  //15000f ;
+        System.out.println("realFloat = " + realFloat);
+    }
+}
