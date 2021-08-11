64ec50a8348de992a5daff902889117999a29e84
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-11 18:50:38 -0500

    Primitivos numericos float

--- src/PrimitivosFloat.java
+++ src/PrimitivosFloat.java
@@ -1,7 +1,32 @@
+import java.sql.SQLOutput;
+
 public class PrimitivosFloat {
+
+    static float varFlotante = 45.45f ;
+
     public static void main(String[] args) {
         
         float realFloat = 1.5e-10f ;  //15000f ;
         System.out.println("realFloat = " + realFloat);
+
+        System.out.println("float Corresponde en byte a = " + Float.BYTES);
+        System.out.println("float Corresponde en bites a = " + Float.SIZE);
+        System.out.println("maximo valor para float = " + Float. MAX_VALUE);
+        System.out.println("minimo valor para float = " + Float. MIN_VALUE);
+
+        System.out.println("-----------------------------------------------------------------------");
+
+        double realDouble = 3.4028235E38 ;
+        System.out.println("realDouble = " + realDouble);
+
+        System.out.println("double Corresponde en byte a = " + Double.BYTES);
+        System.out.println("double Corresponde en bites a = " + Double.SIZE);
+        System.out.println("maximo valor para double = " + Double. MAX_VALUE);
+        System.out.println("minimo valor para double = " + Double. MIN_VALUE);
+
+        System.out.println("-------------------------------------------------------------------------");
+
+        //float varFlotante ;
+        System.out.println("varFlotante = " + varFlotante);
     }
 }
