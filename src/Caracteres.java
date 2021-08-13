7764fbd48e2e7f19971b9461314357647b4d945d
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-12 19:19:22 -0500

    characters

--- src/Caracteres.java
+++ src/Caracteres.java
@@ -12,7 +12,13 @@ public class Caracteres {
         System.out.println("simbolo = " + simbolo);
         System.out.println("simbolo = caracter" + (simbolo == caracter));
 
-        System.out.println("char corresponde en byte= " + Character.BYTES);
+        char espacio = '\u0020' ;
+        char retroceso = '\b' ;
+        char tabulador = '\t' ;
+        char nuevaLinea = '\n' ;
+        char retornoCarro = '\r' ;
+
+        System.out.println("char corresponde en \t byte= " + System.lineSeparator()  + Character.BYTES);
         System.out.println("char corresponde en bites= " + Character.SIZE);
         System.out.println("character  MIN_VALUE = " + Character.MAX_VALUE);
         System.out.println("character  MAX_VALUE = " + Character.MIN_VALUE);
