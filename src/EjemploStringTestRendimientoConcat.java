32243ace46b23d53f7b60725bd3464d5505c94bb
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-17 22:23:24 -0500

    String Validar

--- src/EjemploStringTestRendimientoConcat.java
+++ src/EjemploStringTestRendimientoConcat.java
@@ -11,12 +11,17 @@ public class EjemploStringTestRendimientoConcat {
 
         long inicio = System.currentTimeMillis() ;
 
-            for(int i = 0 ; i < 500 ; i ++ ){
-                // c = c.concat(a).concat(b).concat("\n") ; "" 500 -> 2ms
-                // c += a + b +"\n" ; 500 -> 19
+            for(int i = 0 ; i < 100000 ; i ++ ){
+                c = c.concat(a).concat(b).concat("\n") ; // 500 -> 2ms, 1000 -> 4ms, 10000 -> 6ms, 100000 -> 6.725ms
+                c += a + b +"\n" ; //500 -> 19, 1000 -> 14ms, 10000 -> 74ms,100000 -> 2.277.ms
+                sb.append(a).append(b).append("\n") ; //500 -> 0ms, 100 -> 0ms, 10000 -> 2ms,100000 -> 8ms
+
         }
         long fin = System.currentTimeMillis() ;
         System.out.println(fin - inicio);
         System.out.println("c = " + c);
+        System.out.println("sb.toString() = " + sb.toString());
+
+
     }
 }
