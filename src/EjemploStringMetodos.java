ab55757e55154fb9de843e0bb0d8752940da5a3e
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-17 23:08:37 -0500

    Strings Metodos

---
+++ src/EjemploStringMetodos.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+
+public class EjemploStringMetodos {
+    public static void main(String[] args) {
+        
+        String nombre = "Andres" ;
+
+        System.out.println("nombre.length() = " + nombre.length());
+        System.out.println("nombre.toUpperCase() = " + nombre.toUpperCase());
+        System.out.println("nombre.toLowerCase() = " + nombre.toLowerCase());
+        System.out.println("nombre.equals(\"Andres\") = " + nombre.equals("Andres"));
+        System.out.println("nombre.equals(\"Andres\") = " + nombre.equals("Andres"));
+        System.out.println("nombre.equalsIgnoreCase(\"andres\") = " + nombre.equalsIgnoreCase("andres"));
+        System.out.println("nombre.compareTo(\"Andres\") = " + nombre.compareTo("Andres"));
+        System.out.println("nombre.compareTo(\"Aaron\") = " + nombre.compareTo("Aaron"));
+        System.out.println("nombre.charAt(0) = " + nombre.charAt(0));
+        System.out.println("nombre.charAt(0) = " + nombre.charAt(1));
+        System.out.println("nombre.charAt(0) = " + nombre.charAt(5));
+        System.out.println("nombre.charAt(nombre.length()) = " + nombre.charAt(nombre.length() -1 ));
+
+        System.out.println("nombre.substring(1) = " + nombre.substring(1));
+        System.out.println("nombre.substring(1, 4) = " + nombre.substring(1, 4));
+        System.out.println("fefewfwfew.substring(1, 4) = " + nombre.substring(3, 6));
+        System.out.println("nombre.substring(1, 4) = " + nombre.substring(nombre.length()-4));
+
+    }
+}
