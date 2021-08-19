8b6fb5509a8b2f3cf840c7ba2a26ca83de4995f9
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-18 21:47:22 -0500

    Areglo de errores (decoracion)

--- src/PrimitivosBoolean.java
+++ src/PrimitivosBoolean.java
@@ -4,10 +4,14 @@ public class PrimitivosBoolean {
         boolean datoLogico = true ;
         System.out.println("datoLogico = " + datoLogico) ;
 
+
+
         double d = 98765.43e-3 ; //98,76543e-3
         System.out.println("d = " + d);
         float f = 12345e2f; //123,45
         System.out.println("f = " + f);
+
+
 
         datoLogico = d < f ;
         System.out.println("datoLogico = " + datoLogico);
