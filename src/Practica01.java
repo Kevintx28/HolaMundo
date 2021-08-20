43631894adcb28393fec5fa503b759d05fee67f7
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-19 22:20:14 -0500

    Practica

---
+++ src/Practica01.java
@@ -0,0 +1,16 @@
+public class Practica01 {
+    public static void main(String[] args) {
+        String mensaje = "0853247032753802975043875234809750234587203894572304857234857289345783924758934275089234578974432";
+
+        int posicion = mensaje.length() / 2;
+        System.out.println("MENSAJE = "+mensaje);
+        System.out.println("Tamanio: ["+ mensaje.length() + "]");
+
+        if((mensaje.length() % 2) == 1) {
+            System.out.println("Letra del medio: ["+ mensaje.charAt(posicion)+"]");
+        }
+    }
+}
+
+
+//[1 2 3 4 5 6 7]
