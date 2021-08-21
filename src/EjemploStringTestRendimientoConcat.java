915a6fc378d28c43dbcc7f3fd417e61e6ea5ba7f
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-20 21:43:25 -0500

    Practica01

--- src/EjemploStringTestRendimientoConcat.java
+++ src/EjemploStringTestRendimientoConcat.java
@@ -12,8 +12,8 @@ public class EjemploStringTestRendimientoConcat {
         long inicio = System.currentTimeMillis() ;
 
             for(int i = 0 ; i < 100000 ; i ++ ){
-                c = c.concat(a).concat(b).concat("\n") ; // 500 -> 2ms, 1000 -> 4ms, 10000 -> 6ms, 100000 -> 6.725ms
-                //c += a + b +"\n" ; //500 -> 19, 1000 -> 14ms, 10000 -> 74ms,100000 -> 2.277.ms
+                //c = c.concat(a).concat(b).concat("\n") ; // 500 -> 2ms, 1000 -> 4ms, 10000 -> 6ms, 100000 -> 6.725ms
+                c += a + b +"\n" ; //500 -> 19, 1000 -> 14ms, 10000 -> 74ms,100000 -> 2.277.ms
                 //sb.append(a).append(b).append("\n") ; //500 -> 0ms, 100 -> 0ms, 10000 -> 2ms,100000 -> 8ms
 
         }
@@ -21,8 +21,6 @@ public class EjemploStringTestRendimientoConcat {
         System.out.println(fin - inicio);
         System.out.println("c = " + c);
         System.out.println("sb.toString() = " + sb.toString());
-
-
 
     }
 }
