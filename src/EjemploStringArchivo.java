30f32ce6cf89f21e821ffff34a534cbe87419889
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-23 23:59:37 -0500

    Arreglos

--- src/EjemploStringArchivo.java
+++ src/EjemploStringArchivo.java
@@ -1,9 +1,14 @@
 public class EjemploStringArchivo {
     public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------------------------------------");
+
         String archivo = "alguna_imagen.pdf" ;
         int i = archivo.lastIndexOf(".") ;
         System.out.println("archivo.length() = " + archivo.length());
         System.out.println("archivo.substring(archivo.length(i+1)) = " + archivo.substring(i+1));
+
+        System.out.println("---------------------------------------------------------------------------");
         
     }
 }
