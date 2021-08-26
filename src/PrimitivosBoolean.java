8be40e0a458d346b234ccf0ce0b27dc89c9fd992
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-25 22:07:57 -0500

    ordenando codigo

--- src/PrimitivosBoolean.java
+++ src/PrimitivosBoolean.java
@@ -1,22 +1,23 @@
 public class PrimitivosBoolean {
     public static void main(String[] args) {
 
+        System.out.println("---------------------------------------------------------------");
+
         boolean datoLogico = true ;
         System.out.println("datoLogico = " + datoLogico) ;
-
-
-
         double d = 98765.43e-3 ; //98,76543e-3
         System.out.println("d = " + d);
         float f = 12345e2f; //123,45
         System.out.println("f = " + f);
 
-
+        System.out.println("------------------------------------------------------------------");
 
         datoLogico = d < f ;
         System.out.println("datoLogico = " + datoLogico);
         boolean esIgual = 3-2 == 1 ;
         System.out.println("esIgual = " + esIgual);
+
+        System.out.println("------------------------------------------------------------------");
 
         }
     }
