d43ea78ab62ef31996b34034dbea8788ad78dd28
kevintx28 <kevin.tolachoque@gmail.com>
2021-08-25 22:05:31 -0500

    ordenando codigo

--- src/EjemploStringConcatenacion.java
+++ src/EjemploStringConcatenacion.java
@@ -3,6 +3,7 @@ public class EjemploStringConcatenacion {
         String curso = "Programacion Java" ;
         String profesor = "Andres Guzman" ;
 
+        System.out.println("-----------------------------------------------");
 
         String detalle = curso + "com el instructor " + profesor ;
         System.out.println(detalle);
@@ -10,10 +11,9 @@ public class EjemploStringConcatenacion {
         int numeroA = 10 ;
         int numeroB = 5 ;
         System.out.println(detalle + (numeroA + numeroB)) ;
-
-
         System.out.println( numeroA + numeroB + detalle);
 
+        System.out.println("---------------------------------------------");
 
         String detalle2 = curso.concat(" ".concat(profesor)) ;
         System.out.println("detalle2 = " + detalle2);
