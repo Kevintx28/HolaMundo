24c728bda1726e26fe46339559be0d17f3e9e2d0
kevintx28 <kevin.tolachoque@gmail.com>
2021-09-07 17:36:15 -0500

    Operadores Asignados

---
+++ src/OperadoresAsignacion.java
@@ -0,0 +1,19 @@
+public class OperadoresAsignacion {
+    public static void main(String[] args) {
+
+        int i = 5 ;
+        int j = i + 4 ;
+
+        System.out.println("i = " + j);
+        System.out.println("j = " + j);
+
+        i += 2 ; // i = i + 2
+        System.out.println("i = " + i);
+
+        i += 5 ; // i = i + 5
+        System.out.println("i = " + i);
+
+
+
+    }
+}
