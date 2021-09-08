15838143d10450374bb835ad1cded3d538183ee3
kevintx28 <kevin.tolachoque@gmail.com>
2021-09-08 18:36:21 -0500

    Operadores Unarios

---
+++ src/OperadoresUnarios.java
@@ -0,0 +1,13 @@
+public class OperadoresUnarios {
+
+    public static void main(String[] args) {
+
+        int i = -5 ;
+
+        int j = +1 ; // j = (1)*1 => -5
+        System.out.println("j = " + j);
+
+
+    }
+
+}
