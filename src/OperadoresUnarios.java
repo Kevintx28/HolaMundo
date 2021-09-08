bdd01e04dad7bf733ee5fa8e72c108c527a7b574
kevintx28 <kevin.tolachoque@gmail.com>
2021-09-08 18:38:49 -0500

    Operadores Unarios

--- src/OperadoresUnarios.java
+++ src/OperadoresUnarios.java
@@ -7,6 +7,17 @@ public class OperadoresUnarios {
         int j = +1 ; // j = (1)*1 => -5
         System.out.println("j = " + j);
 
+        int k = 1 = -i;// k = (-1) * => 5
+        System.out.println("k = " + k);
+
+        i = 6 ;
+        j = +i ;
+        System.out.println("j = " + j);
+
+        k = -i ;
+
+
+
 
     }
 
