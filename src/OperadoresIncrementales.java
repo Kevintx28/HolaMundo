bffc47cb909522c1c0664eeafd858301c103d3a8
kevintx28 <kevin.tolachoque@gmail.com>
2021-09-09 18:36:10 -0500

    Operadores Incrementales

--- src/OperadoresIncrementales.java
+++ src/OperadoresIncrementales.java
@@ -1,12 +1,27 @@
 public class OperadoresIncrementales {
     public static void main(String[] args) {
 
+        //Pre Incremento
+
         int i = 1;
         int j = ++ i ; // i = i + 1
 
         System.out.println("i = " + i);
         System.out.println("j = " + j);
 
+        //Post Incremento
+
+        i = 2;
+        j = i++ ;
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+
+        //Pre Decremento
+        i = 3 ;
+        j = --i ;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
 
 
     }
