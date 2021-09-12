e3e4d44ff57da1c1a7f0633f346111ff3e89c9a0
kevintx28 <kevin.tolachoque@gmail.com>
2021-09-11 22:19:18 -0500

    arreglando codigo

--- src/OperadoresAsignacion.java
+++ src/OperadoresAsignacion.java
@@ -1,6 +1,8 @@
 public class OperadoresAsignacion {
     public static void main(String[] args) {
 
+        System.out.println("----------------------------------------------");
+
         int i = 5 ;
         int j = i + 4 ;
 
