952d79abb5035cc4b48d79da1e02603d728982ec
kevintx28 <kevin.tolachoque@gmail.com>
2021-09-14 22:09:48 -0500

    arreglando codigo

--- src/PrimitivosEnteros.java
+++ src/PrimitivosEnteros.java
@@ -1,7 +1,7 @@
 public class PrimitivosEnteros {
     public static void main(String[] args ) {
 
-        byte numeroByte = 127 ;
+        byte numeroByte = 127;
         System.out.println("numeroByte = " + numeroByte);
         System.out.println("tipo byte corresponde en byte a " + Byte.BYTES);
         System.out.println("tipo byte corresponde en byte a " + Byte.SIZE);
@@ -10,7 +10,7 @@ public class PrimitivosEnteros {
 
         System.out.println("-----------------------------------------------------------------------------------");
 
-        short numeroShort = 32767 ;
+        short numeroShort = 32767;
         System.out.println("numeroByte = " + numeroShort);
         System.out.println("tipo short corresponde en byte a " + Short.BYTES);
         System.out.println("tipo short corresponde en byte a " + Short.SIZE);
@@ -19,7 +19,7 @@ public class PrimitivosEnteros {
 
         System.out.println("------------------------------------------------------------------------------------");
 
-        int numeroInt = 2147483647 ;
+        int numeroInt = 2147483647;
         System.out.println("numeroInt =" + numeroInt);
         System.out.println("tipo int corresponde en byte a " + Integer.BYTES);
         System.out.println("tipo int corresponde en byte a " + Integer.SIZE);
@@ -28,8 +28,8 @@ public class PrimitivosEnteros {
 
         System.out.println("------------------------------------------------------------------------------------");
 
-        long numeroLong = 9223372036854775807L ;
-        System.out.println("numerolong = "+ numeroLong );
+        long numeroLong = 9223372036854775807L;
+        System.out.println("numerolong = " + numeroLong);
         System.out.println("tipo Long corresponde en byte a " + Long.BYTES);
         System.out.println("tipo Long corresponde en byte a " + Long.SIZE);
         System.out.println("valor maximo de un Long: " + Long.MAX_VALUE);
@@ -38,5 +38,7 @@ public class PrimitivosEnteros {
         System.out.println("------------------------------------------------------------------------------------");
 
         var numeroVar = 9223372036854775807F;
+
+        System.out.println("----------------------------------------------");
     }
 }
